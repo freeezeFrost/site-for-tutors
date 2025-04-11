a9284e3b61e0bfc31d1326f636f95150b6050627
root <root@nexustime.fun>
2025-04-11 17:31:11 +0300

    🛠 Обновления с сервера: редирект, автообновление сертификатов, remove-unconfirmed.js

---
+++ http-redirect.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.writeHead(301, { "Location": "https://" + req.headers.host + req.url });
+  res.end();
+}).listen(80, () => {
+  console.log('HTTP редирект запущен на порту 80');
+});
