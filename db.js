8065ddd6bc6e92eee0b9a8e3ca0ba1154980f704
Владислав Фокин <main@MacBook-Pro-2.local>
2025-04-14 18:16:44 +0300

    Добавлена база для VDC

--- db.js
+++ db.js
@@ -2,12 +2,21 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
+  user: 'tutors_user',
+  host: 'localhost',
+  database: 'tutors_db',
+  password: 'FokinVlad15',
+  port: 5432,
+}); // ЭТО ДЛЯ ВДС БАЗЫ
+
+
+/* const pool = new Pool({
   database: 'sitefortutors',
   user: 'main',
   password: '', // если не настраивал пароль
   host: 'localhost',
   port: 5432,
-});
+}); */ //ЭТО ДЛЯ БАЗЫ НА ЛОКАЛКЕ
 
 const db = pool;
 export default db;
